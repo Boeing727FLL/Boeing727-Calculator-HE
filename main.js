7fe3ba4fbf2e0823e4c8c0ec5df0cedd8e52dd9c
FunnyIvri <itamarivri.best@gmail.com>
2024-08-09 14:54:20 +0300

    base

---
+++ main.js
@@ -0,0 +1,31 @@
+
+reset()
+function calculate(){
+    const collection = document.getElementsByClassName("misson-buttons");
+    var score = 0
+    Array.from(collection).forEach(div => {
+        const selectElements = div.querySelectorAll('select');
+        selectElements.forEach(selection =>{
+            if (selection.value == "yes"){
+                score = score+parseInt(selection.name)
+            }
+            else if (selection.name == "precision"){
+                score = score+parseInt(selection.value)
+            }
+            else if(!isNaN(Number(selection.value))){
+                    score = score + (parseInt(selection.name)*parseInt(selection.value))
+            }
+        })
+    });
+    document.getElementById("score-display").innerText="SCORE: " + score.toString() 
+    score = 0
+}
+function reset(){
+    const collection = document.getElementsByClassName("misson-buttons");
+    var score = 0
+    Array.from(collection).forEach(div => {
+        const selectElements = div.querySelectorAll('select');
+        selectElements.forEach(selection =>{
+            selection.selectedIndex = 0
+        })})
+}
